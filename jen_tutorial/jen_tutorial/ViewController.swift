1d8396fb05171da095b4f5f769717beee982f7c8
jverbosk <jverbosk@gmail.com>
2017-04-20 13:25:47 -0400

    menu slide out working

---
+++ jen_tutorial/jen_tutorial/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  jen_tutorial
+//
+//  Created by something on 4/20/17.
+//  Copyright © 2017 something. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var myScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        myScrollView.contentSize.height = 1000
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var menuShowing = false
+    
+    @IBAction func openMenu(_ sender: Any) {
+        if (menuShowing) {
+            leadingConstraint.constant = -300
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        menuShowing = !menuShowing
+    }
+
+}
+
