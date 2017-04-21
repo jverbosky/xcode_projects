36651c68ffceeb80021b6acbfb2336bbb1863dad
jverbosky <jverbosk@gmail.com>
2017-04-20 23:43:09 -0400

    added app icon and launch images, still working on launch screen

---
+++ mentor_app/mentor_app/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  mentor_app
+//
+//  Created by something on 4/20/17.
+//  Copyright © 2017 something. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize.height = 1000
+    }
+
+
+
+
+}
+
